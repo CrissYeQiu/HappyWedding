5296e62b2a2c529c1a820b3721243eb9ff9232b6
CrissYeQiu <117644696+CrissYeQiu@users.noreply.github.com>
2026-07-03 08:41:46 +0700

    Happy Wedding

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("openBtn");
+
+btn.onclick = () => {
+
+document.getElementById("cover").style.display="none";
+
+document.getElementById("letter").style.display="block";
+
+}
