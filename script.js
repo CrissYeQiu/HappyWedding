f105fb8f6e4e2e206dca9d241c5e2d9215cad027
CrissYeQiu <117644696+CrissYeQiu@users.noreply.github.com>
2026-07-03 08:50:33 +0700

    Done

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const btn = document.getElementById("openBtn");
 
-btn.onclick = () => {
+btn.onclick = function(){
 
 document.getElementById("cover").style.display="none";
 
 document.getElementById("letter").style.display="block";
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
 
 }
